e9bc7793c07d1eea78726912b0337de8457ec952
zhaoqicheng <643198195@qq.com>
2017-11-17 10:33:59 +0800

    xxx

--- dubbo-client/src/main/java/com/example/demo/DubboClientApplication.java
+++ dubbo-client/src/main/java/com/example/demo/DubboClientApplication.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.dubbo.CityDubboConsumerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class DubboClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DubboClientApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(DubboClientApplication.class, args);
+		CityDubboConsumerService cityService = run.getBean(CityDubboConsumerService.class);
+		cityService.printCity();
 	}
 }
